cdc5cca3aaf4ba876ddfe57f485ec5422e6f9539
Timka5717 <timmka5717@gmail.com>
2025-04-26 12:43:12 +0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+      <h1>Привет! Сайт работает!</h1>
+      <p>Добро пожаловать в Kak-game!</p>
+    </div>
+  );
+}
