11f3d0661013575b7daf80753e90b8949a2bc419
Timka5717 <timmka5717@gmail.com>
2025-04-26 14:36:45 +0500

    Update game.js

--- game.js
+++ game.js
@@ -20,3 +20,25 @@ export default function Game() {
     </div>
   );
 }
+export default function Game() {
+  function startGame() {
+    alert('Игра началась!');
+  }
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Добро пожаловать в игру!</h1>
+      <button
+        onClick={startGame}
+        style={{
+          padding: '10px 20px',
+          fontSize: '18px',
+          cursor: 'pointer',
+          marginTop: '20px'
+        }}
+      >
+        Играть
+      </button>
+    </div>
+  );
+}
