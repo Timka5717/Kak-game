6c3a7548567522fe72ff459f6692992b7a16a61c
Timka5717 <timmka5717@gmail.com>
2025-04-26 13:00:24 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
-    <div style={{ textAlign: 'center', paddingTop: '50px' }}>
+    <div>
       <h1>Привет! Сайт работает!</h1>
-      <p>Добро пожаловать в Kak-game!</p>
     </div>
   );
 }
