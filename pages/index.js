14f727ba51a719b77720bd2084b68d278157d125
Timka5717 <timmka5717@gmail.com>
2025-04-26 13:33:53 +0500

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Добро пожаловать в Kak-Game!</h1>
-      <p>Игра скоро будет здесь!</p>
-    </div>
-  );
-}
--- pages/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Привет! Это kak-game!</h1>
-      <p>Твой проект успешно работает на Vercel!</p>
-    </div>
-  );
-}
